144066cc16768527eac6487666c59a9decf2d85d
ludicorgi <69206279+ludicorgi@users.noreply.github.com>
2023-05-14 21:38:21 +0800

    added survey instructions

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
 import streamlit as st
+from pathlib import Path
+
+def read_markdown_file(markdown_file):
+    return Path(markdown_file).read_text()
 
 st.set_page_config(page_title="ASR Survey", page_icon=":loudspeaker:")
 st.title("Audio Super Resolution Survey")
-st.write("Developed by Raynier Chua, Ivan Lim and Paolo Lojo")
+
+intro_markdown = read_markdown_file("intro.md")
+st.markdown(intro_markdown, unsafe_allow_html=True)
+
+at_yes = "I DO NOT show signs of hearing loss "
+at_no = "I DO show some signs of hearing loss "
+
+at_result = st.radio(
+    "Test result: ",
+    (at_yes, at_no))
+
+st.markdown('### Personal Information')
+
+age = st.number_input('Age', value=18, step=1)
+
+gender = st.selectbox(
+    'Gender',
+    ('Male', 'Female', 'Other'))
+
+st.markdown('If you are exposed to multiple languages at a young age, list your First language as the one you feel more proficient in.')
+
+lang1 = st.selectbox(
+    'First language',
+    ('English', 'Tagalog', 'Other'))
+
+lang2 = st.selectbox(
+    'Second language',
+    ('Tagalog', 'English', 'Other'))
+
+survey_markdown = read_markdown_file("survey.md")
+st.markdown(survey_markdown, unsafe_allow_html=True)
 
 audio_file = open('audio/input.wav', 'rb')
 audio_bytes = audio_file.read()
 
 st.audio(audio_bytes, format='audio/wav')
+
+st.text_input('Enter the words spoken in the audio clip:', 'Text here', disabled=True)
+st.slider('Rate the audio quality:', 1, 5, 3)
+st.markdown('___')
---
+++ survey.md
@@ -0,0 +1,16 @@
+### Audio Evaluation
+The remainder of the survey will show you a series of short 6 to 10 second audio clips. Type the words in the order that you hear in the audio clip. Should some sections of the audio seem unintelligible, you may fill in the gaps with "?". 
+
+Afterwards, rate the quality of the audio clip from 1 (lowest) to 5 (highest). The score should reflect your listening experience. Refer to the list below for your rating criteria.
+
+<ul>
+  <li>1 - Words can not be understood with any feasible effort</li>
+  <li>2 - Words require considerable effort when listening to be understood</li>
+  <li>3 - Words require moderate effort when listening to be understood</li>
+  <li>4 - Words require minimal to no effort when listening to be understood</li>
+  <li>5 - Words can be fully understood even when listening passively or without your attention</li>
+</ul> 
+
+#### Sample Entry
+Here is an example entry for an audio clip containing the words: "..."
+
